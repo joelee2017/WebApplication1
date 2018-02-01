6af5d913320ac2a71f1f3ff5008398c2ee5a36b8
joelee2017 <as955133@gmail.com>
2018-02-01 11:43:26 +0800

    凍結欄位

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace WebApplication1
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryfreezeheader").Include(
+                        "~/Scripts/jquery.freezeheader.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        NorthwindEntities dc = new NorthwindEntities();
+        //網頁無狀態所以，放外層較不影響之後結果
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+                      return View(dc.Customers);
+        }
+
+        public ActionResult Details(string CustomerID)
+        {
+            Customers c = dc.Customers.Find(CustomerID);
+            if (c == null)
+            {
+                return HttpNotFound(); //錯誤時404
+            }
+            return View(c);
+        }
+    }
+}
